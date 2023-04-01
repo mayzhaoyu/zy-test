bca0072edc2802dbdacc14e48fb911544f28dfe4
mayzhaoyu <799722093@qq.com>
2023-04-01 16:20:22 +0800

    Update index.js
    x

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|默|默|生|日|快|乐|');
+      S.UI.simulate('|*|生|日|快|乐|');
     }
 
     S.Drawing.loop(function () {
